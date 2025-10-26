357923f793622a43a9d5dbfa7749d0aa548ded41
Ajay Choudhury <ajaycc17@gmail.com>
2025-10-27 01:33:26 +0530

    add links to apks

--- jsonuploader/urls.py
+++ jsonuploader/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
+    path("", views.display_home, name="display_home"),
     path("uploader/", include("uploader.urls")),
     path("admin/", admin.site.urls),
 ]
